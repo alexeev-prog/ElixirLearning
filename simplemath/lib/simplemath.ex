00863a506bc0147b7f3276bf76e600a7dc37831a
Alexeev Bronislav <alexeev.dev@mail.ru>
2026-05-21 07:52:34 +0700

    update Notes, update examples

--- simplemath/lib/simplemath.ex
+++ simplemath/lib/simplemath.ex
@@ -6,4 +6,27 @@ defmodule Simplemath do
   def pow(a, b) do
     a ** b
   end
+
+  def distance({:point, x1, y1}, {:point, x2, y2}) do
+    x_dist = abs(x1 - x2)
+    y_dist = abs(y1 - y2)
+    :math.sqrt(:math.pow(x_dist, 2) + :math.pow(y_dist, 2))
+  end
+
+  def point_inside_circle?({:point, x, y}, {:circle, {:point, x2, y2}, radius}) do
+    distance({:point, x, y}, {:point, x2, y2}) <= radius
+  end
+
+  def point_inside_rect?({:point, x, y}, {:rect, {:point, x1, y1}, {:point, x2, y2}}) do
+    min_x = min(x1, x2)
+    max_x = max(x1, x2)
+    min_y = min(y1, y2)
+    max_y = max(y1, y2)
+    min_x < x and x < max_x and min_y < y and y < max_y
+  end
+
+  def get_sum_of_first_and_second_item(list) do
+    [first, second | _other] = list
+    first + second
+  end
 end
--- simplemath/run.ex
+++ simplemath/run.ex
@@ -1,2 +1,29 @@
 IO.puts(Simplemath.square(5))
 IO.puts(Simplemath.pow(2, 6))
+
+point = {:point, 50, 50}
+
+# => true
+Simplemath.point_inside_circle?(point, {:circle, {:point, 10, 10}, 100})
+# => false
+Simplemath.point_inside_circle?(point, {:circle, {:point, -10, -10}, 20})
+
+point = {:point, -10, 20}
+circle = {:circle, {:point, 0, 0}, 2}
+rect = {:rect, {:point, 0, 0}, {:point, 2, 2}}
+
+IO.puts(Simplemath.distance({:point, 0, 0}, {:point, 3, 4}))
+
+IO.puts(
+  "Point #{inspect(point)} inside circle #{inspect(circle)}? #{Simplemath.point_inside_circle?({:point, 1, 1}, circle)}"
+)
+
+IO.puts(
+  "Point #{inspect(point)} inside rect #{inspect(rect)}? #{Simplemath.point_inside_rect?({:point, 1, 1}, rect)}"
+)
+
+my_list = [1, 2, 3, 4, 5]
+
+IO.puts(
+  "Sum of first and second item in list #{inspect(my_list)}: #{Simplemath.get_sum_of_first_and_second_item(my_list)}"
+)
