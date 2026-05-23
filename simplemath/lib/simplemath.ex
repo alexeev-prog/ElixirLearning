0c2cfa37e767cd36fd96b98a5c51dc6c19c13e80
Alexeev Bronislav <alexeev.dev@mail.ru>
2026-05-23 21:01:34 +0700

    update simplemath

--- simplemath/lib/simplemath.ex
+++ simplemath/lib/simplemath.ex
@@ -7,6 +7,44 @@ defmodule Simplemath do
     a ** b
   end
 
+  def discriminant(a, b, c) do
+    b * b - 4 * a * c
+  end
+
+  def area_of_circle(radius) do
+    :math.pi() * radius * radius
+  end
+
+  def area_of_rectangle(width, height) do
+    width * height
+  end
+
+  def area_of_triangle(x1, y1, x2, y2, x3, y3) do
+    0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+  end
+
+  def area_of_trapezoid(base1, base2, height) do
+    0.5 * (base1 + base2) * height
+  end
+
+  def solve_square_equation_by_discriminant(a, b, c) do
+    d = discriminant(a, b, c)
+
+    cond do
+      d > 0 ->
+        root1 = (-b + :math.sqrt(d)) / (2 * a)
+        root2 = (-b - :math.sqrt(d)) / (2 * a)
+        {root1, root2}
+
+      d == 0 ->
+        root = -b / (2 * a)
+        {root}
+
+      d < 0 ->
+        :no_real_roots
+    end
+  end
+
   def distance({:point, x1, y1}, {:point, x2, y2}) do
     x_dist = abs(x1 - x2)
     y_dist = abs(y1 - y2)
@@ -23,6 +61,30 @@ defmodule Simplemath do
     min_y = min(y1, y2)
     max_y = max(y1, y2)
     min_x < x and x < max_x and min_y < y and y < max_y
+  end
+
+  def point_inside_triangle?(
+        {:point, x, y},
+        {:triangle, {:point, x1, y1}, {:point, x2, y2}, {:point, x3, y3}}
+      ) do
+    area_abc = area_of_triangle(x1, y1, x2, y2, x3, y3)
+    area_abp = area_of_triangle(x1, y1, x2, y2, x, y)
+    area_acp = area_of_triangle(x1, y1, x3, y3, x, y)
+    area_bcp = area_of_triangle(x2, y2, x3, y3, x, y)
+    area_abc == area_abp + area_acp + area_bcp
+  end
+
+  def point_inside_trapezoid?(
+        {:point, x, y},
+        {:trapezoid, {:point, x1, y1}, {:point, x2, y2}, height}
+      ) do
+    base1 = abs(x1 - x2)
+    base2 = base1 + height
+    area = area_of_trapezoid(base1, base2, height)
+    area_abp = area_of_triangle(x1, y1, x2, y2, x, y)
+    area_acp = area_of_triangle(x1, y1, x + height, y + height, x, y)
+    area_bcp = area_of_triangle(x2, y2, x + height, y + height, x, y)
+    area == area_abp + area_acp + area_bcp
   end
 
   def get_sum_of_first_and_second_item(list) do
--- simplemath/run.ex
+++ simplemath/run.ex
@@ -11,6 +11,9 @@ Simplemath.point_inside_circle?(point, {:circle, {:point, -10, -10}, 20})
 point = {:point, -10, 20}
 circle = {:circle, {:point, 0, 0}, 2}
 rect = {:rect, {:point, 0, 0}, {:point, 2, 2}}
+trapezoid = {:trapezoid, {:point, 0, 0}, {:point, 2, 2}, 2}
+triangle = {:triangle, {:point, 0, 0}, {:point, 2, 0}, {:point, 1, 2}}
+my_list = [1, 2, 3, 4, 5]
 
 IO.puts(Simplemath.distance({:point, 0, 0}, {:point, 3, 4}))
 
@@ -22,8 +25,18 @@ IO.puts(
   "Point #{inspect(point)} inside rect #{inspect(rect)}? #{Simplemath.point_inside_rect?({:point, 1, 1}, rect)}"
 )
 
-my_list = [1, 2, 3, 4, 5]
-
 IO.puts(
   "Sum of first and second item in list #{inspect(my_list)}: #{Simplemath.get_sum_of_first_and_second_item(my_list)}"
+)
+
+IO.puts(
+  "Point #{inspect(point)} inside trapezoid #{inspect(trapezoid)}? #{Simplemath.point_inside_trapezoid?({:point, 1, 1}, trapezoid)}"
+)
+
+IO.puts(
+  "Point #{inspect(point)} inside triangle #{inspect(triangle)}? #{Simplemath.point_inside_triangle?({:point, 1, 1}, triangle)}"
+)
+
+IO.puts(
+  "Roots of equation x^2 - 5x + 6 = 0: #{inspect(Simplemath.solve_square_equation_by_discriminant(1, -5, 6))}"
 )
